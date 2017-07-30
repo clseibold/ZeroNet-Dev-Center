5916a095757b974d3435adcc319d43383501beb5
Christian Seibold <krixano@outlook.com>
2017-07-30 09:18:04 -0500

    Fix links in Our Goal section on homepage

--- js/vueRoutes.js
+++ js/vueRoutes.js
@@ -10,7 +10,17 @@ Vue.component('route-home', {
 				<div class="columns">\
 					<div class="column is-8 is-offset-2">\
 						<h2>Our Goal</h2>\
-						<p>Our Goal is to provide useful information, tutorials, and help to those who want to develop Zites in order to inspire more development on the ZeroNet. This site will <em>not</em> teach the basics of front-end web development. You can go <a href="#">here</a> for that. This site only focuses on ZeroNet-specific development, including the ZeroFrame API. To get started developing a Zite for the ZeroNet, follow <a href="tutorials/?t=the_basics">this tutorial</a> to start learning to build basic zites. You can then move on to other tutorials on the <a href="#">tutorials page</a>.</p>\
+						<p>\
+							Our Goal is to provide useful information, tutorials, and help to those\
+							who want to develop Zites in order to inspire more development on the\
+							ZeroNet. This site will <em>not</em> teach the basics of front-end web\
+							development. You can go <a href="#">here</a> for that. This site only\
+							focuses on ZeroNet-specific development, including the ZeroFrame API. To\
+							get started developing a Zite for the ZeroNet, follow\
+							<route-link to="tutorials/the_basics">this tutorial</route-link> to start\
+							learning to build basic zites. You can then move on to other tutorials on\
+							the <route-link to="tutorials">tutorials page</route-link>.\
+						</p>\
 					</div>\
 				</div>\
 			</section>\
